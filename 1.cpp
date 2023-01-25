1c39b167c98ffce85b043d7012911077cf36d29d
tablelovecpp <123570999+tablelovecpp@users.noreply.github.com>
2023-01-25 23:31:28 +0800

    1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+struct other{
+	int x;
+	int y;
+	int attack;
+	int health;
+	int defence;
+	int xp;
+	int level;
+}Player, bat;
 int main(){
+	Player.x = 0;
+	Player.y = 0;
 	return 0;
 }
